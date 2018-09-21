a5aad79c3d3a73e35265ed0179ca3839e509f26e
fpawel <binf@mail.ru>
2018-09-21 11:17:39 +0300

    remove some types declaration to types.go

--- internal/db/products/currentproduct.go
+++
@@ -1,8 +0,0 @@
-package products
-
-type CurrentProduct struct {
-	Product
-	Checked       bool                `db:"checked"`
-	Comport       string              `db:"comport"`
-	Ordinal       int                 `db:"ordinal"`
-}
--- internal/db/products/product_coefficient_value.go
+++
@@ -1,9 +0,0 @@
-package products
-
-import "github.com/fpawel/ankat/internal/ankat"
-
-type ProductCoefficientValue struct {
-	ProductSerial ankat.ProductSerial
-	Coefficient ankat.Coefficient
-	Value float64
-}
